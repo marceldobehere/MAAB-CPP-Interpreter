202b6d8d3673b23260832d76c8112364454936b9
Masl <marcel.z2006@gmail.com>
2022-10-29 03:25:05 +0200

    ok its done (atleast for the docs rn)

--- MAAB CPP Interpreter/maabTask.h
+++ MAAB CPP Interpreter/maabTask.h
@@ -23,6 +23,56 @@ private:
     uint64_t instrPointer = 0;
     uint32_t defCol = Colors.white;
 
+    uint64_t subLastPos[100];
+    int64_t subDeepness = 0;
+    int64_t maxSubDeepness = 90;
+
+    enum DatatypeNumber : uint8_t
+    {
+        INT = 0,
+        UINT = 1,
+        SHORT = 2,
+        USHORT = 3,
+        LONG = 4,
+        ULONG = 5,
+        CHAR = 6,
+        BOOL = 7,
+        FLOAT = 8,
+        DOUBLE = 9
+    };
+
+    const uint8_t datatypeSizes[10] = 
+    {4,4, 2,2, 8,8, 1,1, 4,8};
+
+    enum OpNumber : uint8_t
+    {
+        PLUS,
+        MINUS,
+        MULT,
+        DIVIDE,
+        MOD,
+        EQUALS,
+        NOT_EQUALS,
+        GREATER,
+        GREATER_EQUALS,
+        LESS,
+        LESS_EQUAL,
+        BOOL_AND,
+        BOOL_OR,
+        BOOL_NOT,
+        BIT_AND,
+        BIT_OR,
+        BIT_NOT,
+        BITSHIFT_LEFT,
+        BITSHIFT_RIGHT
+
+    };
+
+    void Cast(DatatypeNumber typeFrom, uint64_t addrFrom, DatatypeNumber typeTo, uint64_t addrTo);
+    void PrintVal(DatatypeNumber numType, uint64_t numAddr);
+    void ShowBytes(uint8_t amount, uint64_t addr);
+    void Math(OpNumber opNum, DatatypeNumber typeNum, uint64_t addr1, uint64_t addr2, uint64_t addrRes);
+
 
 
 public:
